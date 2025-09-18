1f3847018f9380308ea4357fa968632d5c01c828
Leylammmdl <akuma11tokito@gmail.com>
2025-09-18 15:17:53 +0400

    18 sentyabr tapsiriqlari

---
+++ 🌸🌸🌸NEW LESSONS/19.09.2025.js
@@ -0,0 +1,51 @@
+console.log ("tap 1‼️")
+let i1 = 1;
+while (i1 <= 10) {
+  console.log(i1);
+  i1++;
+}
+console.log ("tap 2‼️")
+
+for (let j = 10; j >= 1; j--) {
+  console.log(j);
+}
+
+
+
+console.log ("tap 3‼️")
+let eded1 = (prompt("eded daxil et")); 
+let cem = 0;
+for (let k = 1; k <= eded1; k++) {
+  cem += k;
+}
+console.log("Cəm:", cem);
+
+console.log ("tap 4‼️")
+let eded2 = 10;
+for (let m = 2; m <= eded2; m += 2) {
+  console.log(m);
+}
+
+console.log ("tap 5‼️")
+let eded3 = 5;
+let faktorial = 1;
+for (let n = 1; n <= eded3; n++) {
+  faktorial *= n;
+}
+console.log(eded3 + "! =", faktorial);
+
+console.log ("tap 6‼️")
+let eded4 = 7;
+let sade = true;
+
+if (eded4 < 2) {
+  sade = false;
+} else {
+  console.log("bilmirem")
+}
+
+if (sade) {
+  console.log(eded4 + " → Sadə ədəddir");
+} else {
+  console.log(eded4 + " → Sadə ədəd deyil");
+}
