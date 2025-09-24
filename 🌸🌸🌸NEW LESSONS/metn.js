47ac82876dc513324d30a046fd1ca02e1dee323e
Leylammmdl <akuma11tokito@gmail.com>
2025-09-24 22:25:34 +0400

    26.09.2025

--- 🌸🌸🌸NEW LESSONS/metn.js
+++ 🌸🌸🌸NEW LESSONS/metn.js
@@ -2,8 +2,9 @@ let text = "Salam.Necesen";
 let index = 0;
 
 for (let i = 0; i < text.length; i++) {
-  if (text[i] !== ".") continue;
-  index = i;
+    if (text[i] !== ".") 
+    continue;
+    index = i;
 }
 
 console.log(index);
