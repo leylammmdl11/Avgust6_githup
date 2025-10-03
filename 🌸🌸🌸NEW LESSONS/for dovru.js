af67cf33d5a821ca985db20635b2a430ad42a0ca
Leylammmdl <akuma11tokito@gmail.com>
2025-10-03 20:11:43 +0400

    Create for dovru.js

---
+++ 🌸🌸🌸NEW LESSONS/for dovru.js
@@ -0,0 +1,13 @@
+let arr = [
+  'alma',
+  'banan',
+  'armud',
+  'çiyələk',
+  'qarpız',
+  'moruq',
+  'qaragilə'
+];
+
+for (let i = 0; i < 7; i++) {
+  console.log(`Meyvə ${i + 1}: ${arr[i]}`);
+}
