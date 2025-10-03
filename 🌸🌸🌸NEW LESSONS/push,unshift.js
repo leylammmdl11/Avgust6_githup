6820b9a2f75afd5faf77e1fa7a5b802e460ca420
Leylammmdl <akuma11tokito@gmail.com>
2025-10-03 21:56:10 +0400

    today

---
+++ 🌸🌸🌸NEW LESSONS/push,unshift.js
@@ -0,0 +1,19 @@
+let array = [
+    'alma',
+    'banan',
+    'armud',
+    'çiyələk',
+    'qarpız',
+    'moruq',
+    'qaragilə']
+array.push ("böyürtkən")
+array.pop()
+array.shift()
+array.unshift("heyva")
+console.log(array)
+
+/////////////////////
+
+let text = '32, 31, 34, 36, 31';
+let newText = text.split(',').join(';');
+console.log(newText);
