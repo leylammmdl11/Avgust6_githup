7e76c3daea866ef84ec3ff2162aa8ef416222e04
Leylammmdl <akuma11tokito@gmail.com>
2025-10-17 18:53:22 +0400

    17.10.2025

---
+++ 🌸🌸🌸NEW LESSONS/17.10.2025.js
@@ -0,0 +1,13 @@
+//
+const student = {
+  name: "Leyla",
+  age: 14,
+  grade: 8
+};
+console.log(student);
+//
+student.grade = 9;
+console.log(student.grade);
+//
+student.school = "89 nömrəli məktəb";
+console.log(student);
